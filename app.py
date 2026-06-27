2187bd5f4e2d39aeb973f1bd5f1c3c23115595de
Ahmed-bd7 <ahmed.21@windowslive.com>
2026-06-27 19:59:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -271,6 +271,14 @@ def get_internal_matches():
 {"id": 70, "team_home": "الكونغو الديمقراطية", "team_away": "أوزبكستان", "time": datetime(2026, 6, 28, 2, 30, tzinfo=ksa_tz)},
 {"id": 71, "team_home": "الجزائر", "team_away": "النمسا", "time": datetime(2026, 6, 28, 5, 0, tzinfo=ksa_tz)},
 {"id": 72, "team_home": "الأردن", "team_away": "الأرجنتين", "time": datetime(2026, 6, 28, 5, 0, tzinfo=ksa_tz)},
+# ===== دور الـ32 =====
+{"id": 73, "team_home": "كندا", "team_away": "جنوب أفريقيا", "time": datetime(2026, 6, 28, 22, 0, tzinfo=ksa_tz)},
+{"id": 74, "team_home": "اليابان", "team_away": "البرازيل", "time": datetime(2026, 6, 29, 20, 0, tzinfo=ksa_tz)},
+{"id": 75, "team_home": "المغرب", "team_away": "هولندا", "time": datetime(2026, 6, 30, 4, 0, tzinfo=ksa_tz)},
+{"id": 76, "team_home": "النرويج", "team_away": "ساحل العاج", "time": datetime(2026, 6, 30, 20, 0, tzinfo=ksa_tz)},
+{"id": 77, "team_home": "السويد", "team_away": "فرنسا", "time": datetime(2026, 7, 1, 0, 0, tzinfo=ksa_tz)},
+{"id": 78, "team_home": "الولايات المتحدة", "team_away": "البوسنة والهرسك", "time": datetime(2026, 7, 2, 3, 0, tzinfo=ksa_tz)},
+{"id": 79, "team_home": "الأرجنتين", "team_away": "الرأس الأخضر", "time": datetime(2026, 7, 4, 1, 0, tzinfo=ksa_tz)},
     ]
 
 all_matches = get_internal_matches()
